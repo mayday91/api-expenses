4920902dc1a754369081ae00a8cbf6f3a7976ac5
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-09-10 17:37:30 -0400

    commented in heroku deployment stuff in server.js

--- server.js
+++ server.js
@@ -48,14 +48,14 @@ app.use(
 const port = process.env.PORT || serverDevPort || 5000
 
 // for Heroku deployment
-// if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV ===' staging') {
-// 	app.use(express.static('client/build'));
-// 	app.get('*', (req, res) => {
-// 	res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// 	});
-//  }
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV ===' staging') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+	});
+ }
 
-//  const path = require("path");
+ const path = require("path");
 
 
 
