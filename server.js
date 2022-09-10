69e46f5c395a8fe2347fe72dbdfe3f6e790403e2
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-09-10 16:58:53 -0400

    modified vercel.json

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const auth = require('./lib/auth')
 
 // define server and client ports
 // used for cors and local port declaration
-const serverDevPort = 3001
+const serverDevPort = 8000
 const clientDevPort = 3000
 
 // establish database connection
