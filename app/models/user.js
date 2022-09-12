4721b4321419674fb5a41e50f5d678d862deef77
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-09-11 23:51:41 -0400

    added username to sign up and sign in

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema(
 	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
 		email: {
 			type: String,
 			required: true,
