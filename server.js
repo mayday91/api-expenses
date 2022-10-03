c0e17c34d1f3da0388b90915253208e98029cf31
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-10-03 14:50:42 -0400

    added new function on line 69 of server.js to hopefully stop the errors with CORS while deployed

--- server.js
+++ server.js
@@ -66,6 +66,15 @@ app.use((req, res, next) => {
 	next();
 });
 
+////////// CORS ACCESS //////////////////////
+/////////////////////////////////////////////////
+app.all('*', function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+////////////////////////////////////////////////
 
 // this middleware makes it so the client can use the Rails convention
 // of `Authorization: Token token=<token>` OR the Express convention of
