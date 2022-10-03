6b5d1571e454fedd7fb2c034e1f637f6b7442f7b
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-10-03 15:24:25 -0400

    changed the client origin on line 42 of server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const app = express()
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 app.use(
 	cors({
-		origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`,
+		origin:"https://pecunia.netlify.app/accounts/sign-in"||`http://localhost:${clientDevPort}`,
 	})
 )
 
